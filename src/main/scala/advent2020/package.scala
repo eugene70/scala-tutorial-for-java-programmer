c7ba7f2b0e31cf90d8de540849c1a7587b8286f1
Eugene Kim <eugene70kim@gmail.com>
2020-12-07 19:59:56 +0900

    Day5 Completed!!

--- src/main/scala/advent2020/package.scala
+++ src/main/scala/advent2020/package.scala
@@ -1,8 +1,9 @@
 package object advent2020 {
-  def time(name: String, fn: () => Any): Unit = {
+  def time[R](name: String, fn: () => R): R = {
     val startMillis = System.currentTimeMillis()
     val result = fn()
     val endMillis = System.currentTimeMillis()
     println(s"$name result: $result, time: ${endMillis - startMillis} ms")
+    result
   }
 }
